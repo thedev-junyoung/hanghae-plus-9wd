922871ac058a8054c1ac5d9c3e48a0cf0898d0a9
thedev-junyoung <thedev.junyoung@gmail.com>
2025-05-29 03:06:46 +0900

    feat: @EnableKafka 추가

--- src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -3,9 +3,11 @@ package kr.hhplus.be.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableCaching
+@EnableKafka
 public class ServerApplication {
 
 	public static void main(String[] args) {
