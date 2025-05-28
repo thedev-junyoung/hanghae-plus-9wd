773c4551bb79f7e9d11df94027210aa1a9a2826c
thedev-junyoung <thedev.junyoung@gmail.com>
2025-05-29 04:07:55 +0900

    refactor: 카프카 이벤트 기능추가로 인한 @EmbeddedKafka 추가

--- src/test/java/kr/hhplus/be/server/application/order/OrderServiceIntegrationTest.java
+++ src/test/java/kr/hhplus/be/server/application/order/OrderServiceIntegrationTest.java
@@ -6,13 +6,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
 
 @SpringBootTest
+@EmbeddedKafka(partitions = 1, topics = {"order-export"})
 class OrderServiceIntegrationTest {
 
     @Autowired
