f26aa5a9ba915d0ce522423981604ed87f00047f
thedev-junyoung <thedev.junyoung@gmail.com>
2025-05-30 01:10:49 +0900

    config: 재고 관련 카프카 설정

--- src/main/java/kr/hhplus/be/server/config/KafkaConfig.java
+++ src/main/java/kr/hhplus/be/server/config/KafkaConfig.java
@@ -1,6 +1,8 @@
 package kr.hhplus.be.server.config;
 
 import kr.hhplus.be.server.domain.orderexport.OrderExportPayload;
+import kr.hhplus.be.server.infrastructure.kafka.StockDecreaseFailedKafkaMessage;
+import kr.hhplus.be.server.infrastructure.kafka.StockDecreaseRequestedKafkaMessage;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
@@ -76,6 +78,70 @@ public class KafkaConfig {
         return factory;
     }
 
+    // === StockDecreaseRequestedKafkaMessage Producer and Consumer ===
+
+    @Bean
+    public ProducerFactory<String, StockDecreaseRequestedKafkaMessage> stockDecreaseProducerFactory(KafkaProperties props) {
+        return KafkaFactoryBuilder.buildProducerFactory(props, JsonSerializer.class);
+    }
+
+    @Bean
+    public KafkaTemplate<String, StockDecreaseRequestedKafkaMessage> stockDecreaseKafkaTemplate(
+            ProducerFactory<String, StockDecreaseRequestedKafkaMessage> factory
+    ) {
+        return KafkaFactoryBuilder.buildKafkaTemplate(factory);
+    }
+
+    @Bean
+    public ConsumerFactory<String, StockDecreaseRequestedKafkaMessage> stockDecreaseConsumerFactory(KafkaProperties props) {
+        return KafkaFactoryBuilder.buildConsumerFactory(
+                props,
+                JsonDeserializer.class,
+                StockDecreaseRequestedKafkaMessage.class
+        );
+    }
+
+    @Bean(name = "stockDecreaseKafkaListenerFactory")
+    public ConcurrentKafkaListenerContainerFactory<String, StockDecreaseRequestedKafkaMessage> stockDecreaseKafkaListenerFactory(
+            ConsumerFactory<String, StockDecreaseRequestedKafkaMessage> consumerFactory
+    ) {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, StockDecreaseRequestedKafkaMessage>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties().setAckMode(org.springframework.kafka.listener.ContainerProperties.AckMode.MANUAL);
+        return factory;
+    }
+
+    // === StockDecreaseFailedKafkaMessage Producer and Consumer ===
+
+    @Bean
+    public ProducerFactory<String, StockDecreaseFailedKafkaMessage> stockDecreaseFailedProducerFactory(KafkaProperties props) {
+        return KafkaFactoryBuilder.buildProducerFactory(props, JsonSerializer.class);
+    }
+
+    @Bean
+    public KafkaTemplate<String, StockDecreaseFailedKafkaMessage> stockDecreaseFailedKafkaTemplate(
+            ProducerFactory<String, StockDecreaseFailedKafkaMessage> factory
+    ) {
+        return KafkaFactoryBuilder.buildKafkaTemplate(factory);
+    }
+
+    @Bean
+    public ConsumerFactory<String, StockDecreaseFailedKafkaMessage> stockDecreaseFailedConsumerFactory(KafkaProperties props) {
+        return KafkaFactoryBuilder.buildConsumerFactory(
+                props,
+                JsonDeserializer.class,
+                StockDecreaseFailedKafkaMessage.class
+        );
+    }
+    @Bean(name = "stockDecreaseFailedKafkaListenerFactory")
+    public ConcurrentKafkaListenerContainerFactory<String, StockDecreaseFailedKafkaMessage> stockDecreaseFailedKafkaListenerFactory(
+            ConsumerFactory<String, StockDecreaseFailedKafkaMessage> consumerFactory
+    ) {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, StockDecreaseFailedKafkaMessage>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties().setAckMode(org.springframework.kafka.listener.ContainerProperties.AckMode.MANUAL);
+        return factory;
+    }
 
 
     // === Common Error Handler ===
